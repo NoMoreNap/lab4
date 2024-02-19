fb8ccf23d5a75dfe52bf9c7ce0e8a13743c3c4f6
NoMoreNap <vv2trash@gmail.com>
2024-02-19 12:27:34 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -2,25 +2,29 @@
 
 using  namespace  std;
 
-long int Fibonacci(int n) {
-
+int Fibonacci(int n) {
 
     if (n == 1) {
         return 0;
     } else if (n == 2) {
         return 1;
     } else {
-        int a = Fibonacci(n - 1) + Fibonacci(n  - 2);
+        int num1 = Fibonacci(n - 1);
+        int num2 = Fibonacci(n  - 2);
+        int a = num1  + num2;
+        if (a > 2e9 + 148e6 + 1) {
+            cout << a;
+        }
         return a;
     }
 
 }
 
-
 int main() {
     setlocale(LC_ALL, "Russian");
+    int step = 4;
 
-    int b = Fibonacci(4);
+    int b = Fibonacci(step);
 
     cout << b;
 
